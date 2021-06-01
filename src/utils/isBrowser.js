a98ca1710b5346b3188214d993c66eca91937b19
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2021-06-01 19:29:16 +0200

    #19 remove isBrowser helper, use a function. Remove unecesary defaults

--- src/utils/isBrowser.js
+++
@@ -1,4 +0,0 @@
-const isNodejs = (typeof process !== 'undefined' && process.versions && process.versions.node)
-const isBrowser = !isNodejs && (typeof window !== 'undefined' || typeof self !== 'undefined')
-
-module.exports = isBrowser

0863984972157e3d1eef729fbd303dfff27170b6
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2021-06-02 18:03:46 +0200

    #21 add default serviceURL

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,8 @@ const AEM_GRAPHQL_ACTIONS = {
   persist: 'graphql/persist.json',
   execute: 'graphql/execute.json',
   list: 'graphql/list.json',
-  endpoint: 'content/graphql/global/endpoint.json'
+  endpoint: 'content/graphql/global/endpoint.json',
+  serviceURL: '/'
 }
 
 module.exports = {
